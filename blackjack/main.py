2e00507853f9013d5ad6c390d184c8aaa10289da
Sorin <fratean.em.sorin@student.utcluj.ro>
2022-09-27 19:18:40 +0300

    update with betting system

--- blackjack/main.py
+++ blackjack/main.py
@@ -28,6 +28,31 @@ def score(list):
     return sum
 
 
+def final_touch(player_cards, player_score, computer_cards, computer_score, bet, balance):
+    print(f"Your final cards: {player_cards}, score: {player_score}")
+    while computer_score <= 16 and player_score < 22:
+        computer_cards = hit_card(computer_cards)
+        computer_score = score(computer_cards)
+    print(f"Computer's final cards: {computer_cards}, score: {computer_score}")
+    if computer_score > 21 and player_score > 21:
+        print("Both went over. Draw.")
+    elif computer_score > 21:
+        print("Opponent went over. You win!")
+        balance = balance + bet
+    elif 22 > computer_score > player_score:
+        print("You lost.")
+        balance = balance - bet
+    elif computer_score == player_score:
+        print("It's a draw.")
+    elif player_score > 21 and computer_score < 22:
+        print("You lost.")
+        balance = balance - bet
+    else:
+        print("You win!")
+        balance = balance + bet
+    return balance
+
+
 def main():
     choice = True
     play = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
@@ -36,7 +61,14 @@ def main():
         return
     else:
         print(logo)
-    while choice:
+        balance = int(input("Enter your balance: $"))
+        print(f"Your balance is ${balance}.")
+    while balance > 0:
+        bet = int(input("Place your bet: $"))
+        print(balance)
+        if bet > balance:
+            print(f"Not enough credits. Your balance is ${balance}.")
+        win = False
         computer_score = 0
         player_cards = shuffle_cards(2)
         computer_cards = shuffle_cards(2)
@@ -52,19 +84,20 @@ def main():
         print(f"Computer's first card: {computer_cards[0]}")
         another_card = input("Type 'y' to get another card, type 'n' to pass: ")
         if another_card == 'n':
-            print(f"Your final cards: {player_cards}, score: {player_score}")
-            while computer_score <= 16:
-                computer_cards = hit_card(computer_cards)
-                computer_score = score(computer_cards)
-            print(f"Computer's final cards: {computer_cards}, score: {computer_score}")
-            if computer_score > 21:
-                print("Opponent went over. You win.")
-            elif 22 > computer_score > player_score:
-                print("You lost.")
-            elif computer_score == player_score:
-                print("It's a draw.")
-            else:
-                print("You win!")
+            balance = final_touch(player_cards, player_score, computer_cards, computer_score, bet, balance)
+            print(f"Your balance is ${balance}.")
+        elif another_card == 'y':
+            while player_score < 22:
+                player_cards = hit_card(player_cards)
+                player_score = score(player_cards)
+                print(f"Your cards: {player_cards}, current score: {player_score}")
+                print(f"Computer's first card: {computer_cards[0]}")
+                if player_score < 21:
+                    another_card = input("Type 'y' to get another card, type 'n' to pass: ")
+                    if another_card == 'n':
+                        break
+            balance = final_touch(player_cards, player_score, computer_cards, computer_score, bet, balance)
+            print(f"Your balance is ${balance}.")
 
 
 if __name__ == '__main__':
