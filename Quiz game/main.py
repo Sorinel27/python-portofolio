87c781db091efd26739b8b4f797daf705910b746
Sorin <fratean.em.sorin@student.utcluj.ro>
2022-10-11 01:00:57 +0300

    add quiz game

---
+++ Quiz game/main.py
@@ -0,0 +1,22 @@
+import random
+from question_model import Question
+from question_data import data
+from quiz_brain import QuizBrain
+
+
+def main():
+    question_bank = []
+    for item in data:
+        q = item["question"]
+        a = item["correct_answer"]
+        obj = Question(q, a)
+        question_bank.append(obj)
+    quiz = QuizBrain(question_bank)
+    while quiz.still_has_question():
+        quiz.next_question()
+    print("\nYou've completed the quiz.")
+    print(f"Your final score was: {quiz.score}/{quiz.question_number}.")
+
+
+if __name__ == "__main__":
+    main()
