b0d67e19aa0304d353a891b21eb5da64b26698d8
Sorinel27 <sorin78910@gmail.com>
2022-11-09 13:45:57 +0200

    Update with the user removing the words has checked right, the remaining words to be put in a separate csv named words_to_learn.csv

--- flash card/main.py
+++ flash card/main.py
@@ -8,11 +8,15 @@ list_words = []
 new_list = []
 data_dict = {"Romanian": [], "English": []}
 BACKGROUND_COLOR = "#B1DDC6"
+LABEL_BACKGROUND_COLOR = "#91C2AF"
 ro = ""
 en = ""
+WORDS_NUMBER = 0
+FILE_NEW = "ro_words.csv"
+FILE_OLD = "words_to_learn.csv"
 
 """
-this function takes the most used romanian words in movie/tv series subtitles
+This function takes the most used romanian words in movie/tv series subtitles,
 and puts them in a csv file along with their translation in english
 """
 
@@ -21,7 +25,8 @@ def from_txt_to_csv():
     global list_words
     global new_list
     global data_dict
-    nr = 0
+    global WORDS_NUMBER
+    nr = -1
     with open("ro.txt", "r+", encoding="utf8") as file:
         for i in file:
             nr += 1
@@ -30,6 +35,7 @@ def from_txt_to_csv():
             if nr == 5000:
                 break
         file.close()
+    WORDS_NUMBER = nr
     for row in list_words:
         word = ""
         ok = True
@@ -50,45 +56,105 @@ def from_txt_to_csv():
 
 
 def wrong_choice():
+    print(WORDS_NUMBER)
+    window.after_cancel(window)
     global ro
     global en
     col_names = ['Romanian', 'English']
     ro_words = []
     en_words = []
-    words_data = pd.read_csv('ro_words.csv')
+    if FILE_OLD:
+        file = FILE_OLD
+    else:
+        file = FILE_NEW
+    try:
+        words_data = pd.read_csv(file)
+    except FileNotFoundError:
+        words_data = pd.read_csv(FILE_NEW)
     for item in words_data[col_names[0]]:
         ro_words.append(item)
     for item in words_data[col_names[1]]:
         en_words.append(item)
-    random_index = random.randint(0, 5001)
+    random_index = random.randint(0, WORDS_NUMBER)
+    print(random_index)
     ro = ro_words[random_index]
     en = en_words[random_index]
-    label_word.config(text=ro_words[random_index])
+    canvas.itemconfig(canvas_image, image=front_card)
+    label_word.config(text=ro, bg="white", fg="black")
+    label_language.config(text="Romanian", bg="white", fg="black")
+    window.after(3000, change_card)
 
 
 def right_choice():
+    print(WORDS_NUMBER)
+    window.after_cancel(window)
     global ro
     global en
+    global data_dict
     col_names = ['Romanian', 'English']
     ro_words = []
     en_words = []
-    words_data = pd.read_csv('ro_words.csv')
+    data_dict[col_names[0]].remove(ro)
+    data_dict[col_names[1]].remove(en)
+    new_data = pd.DataFrame(data_dict)
+    new_data.to_csv("words_to_learn.csv", index=False, encoding="utf-8-sig")
+    if FILE_OLD:
+        file = FILE_OLD
+    else:
+        file = FILE_NEW
+    try:
+        words_data = pd.read_csv(file)
+    except FileNotFoundError:
+        words_data = pd.read_csv(FILE_NEW)
     for item in words_data[col_names[0]]:
         ro_words.append(item)
     for item in words_data[col_names[1]]:
         en_words.append(item)
-    random_index = random.randint(0, 5001)
+    print(WORDS_NUMBER)
+    random_index = random.randint(0, WORDS_NUMBER)
+    print(random_index)
     ro = ro_words[random_index]
     en = en_words[random_index]
-    label_word.config(text=ro_words[0])
+    canvas.itemconfig(canvas_image, image=front_card)
+    label_word.config(text=ro, bg="white", fg="black")
+    label_language.config(text="Romanian", bg="white", fg="black")
+    window.after(3000, change_card)
 
 
 def change_card():
     global en
     global ro
     canvas.itemconfig(canvas_image, image=back_card)
-    label_word.config(text=en, bg=BACKGROUND_COLOR)
-    label_language.config(text="English", bg=BACKGROUND_COLOR)
+    label_word.config(text=en, bg=LABEL_BACKGROUND_COLOR, fg="white")
+    label_language.config(text="English", bg=LABEL_BACKGROUND_COLOR, fg="white")
+
+
+def random_word():
+    global ro
+    global en
+    global WORDS_NUMBER
+    global data_dict
+    col_names = ['Romanian', 'English']
+    ro_words = []
+    en_words = []
+    if FILE_OLD:
+        file = FILE_OLD
+    else:
+        file = FILE_NEW
+    try:
+        words_data = pd.read_csv(file)
+    except FileNotFoundError:
+        words_data = pd.read_csv(FILE_NEW)
+    for item in words_data[col_names[0]]:
+        WORDS_NUMBER += 1
+        ro_words.append(item)
+        data_dict[col_names[0]].append(item)
+    for item in words_data[col_names[1]]:
+        en_words.append(item)
+        data_dict[col_names[1]].append(item)
+    random_index = random.randint(0, WORDS_NUMBER)
+    ro = ro_words[random_index]
+    en = en_words[random_index]
 
 
 # uncomment the line below if you don't have the ro-en csv file
@@ -108,7 +174,8 @@ canvas.grid(column=0, row=0, columnspan=2)
 label_language = Label(text="Romanian", font=("Ariel", 40, "italic"), bg="white")
 label_language.place(anchor='center', x=400, y=150)
 
-label_word = Label(text="apus", font=("Ariel", 60, "bold"), bg="white")
+random_word()
+label_word = Label(text=ro, font=("Ariel", 60, "bold"), bg="white")
 label_word.place(anchor='center', x=400, y=263)
 
 image_button_wrong = PhotoImage(file="images/wrong.png")
