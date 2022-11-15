e1b3901b45165d2beabf3149f5a63645aabb7245
Sorinel27 <sorin78910@gmail.com>
2022-11-16 01:33:47 +0200

    update weather alert with sms notification

--- Weather alert/main.py
+++ Weather alert/main.py
@@ -1,7 +1,11 @@
 import requests
+from twilio.rest import Client
 
-API_KEY = "1909eb736e25a842f48193695c16d214"
+API_KEY = "1909eb736e25a842f48193695c16d214"  # not so secret info
 # Latitude and longitude for Cluj_Napoca, RO
+
+account_sid = 'AC75b71ff80d7412ed3f9a899bd27ddd7d'
+auth_token = 'top secret info'
 
 LAT = 46.771210
 LONG = 23.623634
@@ -10,14 +14,25 @@ parameters = {
     "lat": LAT,
     "lon": LONG,
     "appid": API_KEY,
+    "exclude": "current,minutely,daily",
     "units": "metric"
 }
 
-response = requests.get("https://api.openweathermap.org/data/2.5/weather", params=parameters)
+response = requests.get("https://api.openweathermap.org/data/2.5/forecast", params=parameters)
 response.raise_for_status()
-data = dict(response.json())
-weather_data = data['weather']
-temp_data = data['main']
+data = response.json()
 
-print(f"The weather for {data['name']} is {weather_data[0]['main']} with a temperature of {temp_data['temp']}°C")
-#to be contined...
+client = Client(account_sid, auth_token)
+
+# Using the free api key we can't get the weather data hourly but at 4 hours.
+# This will result in the first 8 items of "list"
+forecast_list = data['list']
+for i in range(8):
+    if forecast_list[i]['weather'][0]['id'] < 700:
+        message = client.messages.create(
+            body='Raining today!',
+            from_='+15017122661',
+            to='+40761468119'
+        )
+        print(message.status)
+        break
