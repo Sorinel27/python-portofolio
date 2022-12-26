7416ee6c087d7917725ea604240967fbf5f9e440
Sorinel27 <sorin78910@gmail.com>
2022-12-26 20:55:11 +0200

    added Instagram follower bot

---
+++ Instagram follower bot/main.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+import time
+
+URL = "https://www.instagram.com/"
+USER = ""
+INSTA_URL = "https://www.instagram.com/sorinel_fr/followers/"
+
+email = "soringrape@gmail.com"
+password = "carsorin"
+
+driver = webdriver.Chrome(executable_path="/Users/Sorin/PycharmProjects/100 days/day48/chromedriver")
+driver.get(URL)
+
+cookies = driver.find_element(by="xpath", value='/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/button[2]')
+cookies.click()
+time.sleep(1)
+log_input = driver.find_element(by='xpath', value='/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[1]/div/label/input')
+log_input.send_keys(email)
+time.sleep(1)
+log_input = driver.find_element(by='xpath', value='/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[2]/div/label/input')
+log_input.send_keys(password)
+time.sleep(1)
+log_in = driver.find_element(by='xpath', value='/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button')
+log_in.click()
+time.sleep(3)
+
+driver.get(INSTA_URL)
+time.sleep(3)
+number_of_people_to_follow = 100
+
+for i in range(number_of_people_to_follow):
+    follow = driver.find_element(by='xpath', value=f'/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[1]/div/div[{i + 1}]/div[3]/button/div/div')
+    follow.click()
+    time.sleep(1)
+
+driver.quit()
