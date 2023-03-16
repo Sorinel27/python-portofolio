97a710ef302a9c0cf1b3da46bace89103452e7c7
Sorinel27 <sorin78910@gmail.com>
2023-03-16 20:57:13 +0200

    update

--- Blog site with Flask/server.py
+++ Blog site with Flask/server.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 import requests
 
-
 app = Flask(__name__)
 response = requests.get('https://api.npoint.io/c790b4d5cab58020d391')
 response = response.json()
+
 
 @app.route('/')
 def home_page():
@@ -16,10 +16,19 @@ def blog_page():
     return render_template('blog.html', posts=response, id=0)
 
 
-@app.route('/post/<post_id>')
-def post_page(post_id):
-    return render_template('blog.html', posts=response, id=post_id)
-
+@app.route('/post/<id>')
+def post_page(id):
+    global response
+    info = []
+    id = int(id)
+    response = requests.get('https://api.npoint.io/c790b4d5cab58020d391')
+    response = response.json()
+    for item in response:
+        if int(item['id']) == id:
+            info.append(item['title'])
+            info.append(item['subtitle'])
+            info.append(item['body'])
+    return render_template('blog.html', title=info[0], subtitle=info[1], body=info[2], id=1)
 
 
 if __name__ == '__main__':
