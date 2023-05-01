1ff11efa48bbc14c1d321bd7018c7ca42fc1bd68
Sorin <sorin78910@gmail.com>
2023-05-02 00:42:07 +0300

    update

--- Web app with Flask/server.py
+++ Web app with Flask/server.py
@@ -9,5 +9,20 @@ def main_page():
     return render_template('index.html')
 
 
+@app.route('/blog')
+def blog_page():
+    return render_template('blog.html')
+
+
+@app.route('/about')
+def about_page():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact_page():
+    return render_template('contact.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
