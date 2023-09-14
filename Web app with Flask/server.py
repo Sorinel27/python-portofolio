8657710f6b19595de5883690047bc8f76e3a9013
Sorin <sorin78910@gmail.com>
2023-09-14 13:20:46 +0300

    added user authentication with login/logout, the ability to create a new post only if you are logged in

--- Web app with Flask/server.py
+++ Web app with Flask/server.py
@@ -1,11 +1,13 @@
 import requests
 import smtplib
-from flask import Flask, render_template, url_for, request, redirect
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, render_template, url_for, request, redirect, flash
 from flask_wtf import FlaskForm, CSRFProtect
 from flask_ckeditor import CKEditorField, CKEditor
 from flask_sqlalchemy import SQLAlchemy
 from wtforms.validators import DataRequired
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, EmailField
 from flask_bootstrap import Bootstrap
 import datetime
 
@@ -17,11 +19,18 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 csrf = CSRFProtect(app)
 app.secret_key = "topsecretkey"
 ckeditor = CKEditor(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 with app.app_context():
     Bootstrap(app)
     db = SQLAlchemy(app)
 
+    class ContactForm(FlaskForm):
+        name = StringField('Your name', validators=[DataRequired()])
+        email = EmailField('Your e-mail', validators=[DataRequired()])
+        text = StringField('Your message', validators=[DataRequired()])
+        submit = SubmitField('Send')
 
     class PostForm(FlaskForm):
         title = StringField('Blog Post Title', validators=[DataRequired()])
@@ -30,6 +39,11 @@ with app.app_context():
         content = CKEditorField('Content', validators=[DataRequired()])
         submit = SubmitField('Submit post')
 
+    class User(UserMixin, db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        email = db.Column(db.String(100), unique=True)
+        password = db.Column(db.String(100))
+        name = db.Column(db.String(1000), unique=True)
 
     class Post(db.Model):
         id = db.Column(db.Integer, primary_key=True)
@@ -46,9 +60,13 @@ with app.app_context():
     db.create_all()
 
 
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.filter_by(id=user_id).first()
+
     @app.route('/')
     def main_page():
-        return render_template('index.html')
+        return render_template('index.html', current_user=current_user)
 
 
     @app.route('/news')
@@ -56,7 +74,7 @@ with app.app_context():
         response = requests.get(GET_LINK)
         response = response.json()
         number_of_articles = len(response['articles'])
-        return render_template('news.html', response=response, number_of_articles=number_of_articles)
+        return render_template('news.html', response=response, number_of_articles=number_of_articles, is_logged=current_user.is_authenticated)
 
 
     @app.route('/news/post/<post_id>')
@@ -66,12 +84,12 @@ with app.app_context():
         number_of_articles = len(response['articles'])
         post_id = int(post_id)
         return render_template('news-post.html', response=response, number_of_articles=number_of_articles,
-                               post_id=post_id)
+                               post_id=post_id, )
 
 
     @app.route('/about')
     def about_page():
-        return render_template('about.html')
+        return render_template('about.html', is_logged=current_user.is_authenticated)
 
 
     @app.route('/messageSend/<name>/<email>/<message>')
@@ -96,31 +114,30 @@ with app.app_context():
 
     @app.route('/contact', methods=['POST', 'GET'])
     def contact_page():
-        data = []
+        form = ContactForm()
         if request.method == 'POST':
-            data.append(request.form['name'])
             name = request.form['name']
-            data.append(request.form['email'])
             email = request.form['email']
-            data.append(request.form['message'])
-            message = request.form['message']
+            message = request.form['text']
+            print(message)
             return redirect(url_for('message_send', name=name, email=email, message=message))
-        return render_template('contact.html')
+        return render_template('contact.html', form=form)
 
 
     @app.route('/blog')
     def blog_page():
         all_posts = db.session.query(Post).all()
-        return render_template('blog.html', posts=all_posts)
+        return render_template('blog.html', posts=all_posts, is_logged=current_user.is_authenticated)
 
 
     @app.route('/blog/post/<id>')
     def post_page(id):
         selected_post = Post.query.get(id)
-        return render_template('post.html', post=selected_post)
+        return render_template('post.html', post=selected_post, current_user=current_user)
 
 
     @app.route('/new-post', methods=['POST', 'GET'])
+    @login_required
     def new_post():
         form = PostForm()
         if request.method == "POST":
@@ -134,7 +151,7 @@ with app.app_context():
             new_post = Post(
                 title=request.form['title'],
                 subtitle=request.form['subtitle'],
-                user=request.form['user'],
+                user=current_user.name,
                 date=today_date,
                 text=request.form['content']
             )
@@ -157,7 +174,7 @@ with app.app_context():
         if request.method == 'POST':
             post_data.title = request.form['title']
             post_data.subtitle = request.form['subtitle']
-            post_data.user = request.form['user']
+            post_data.user = current_user.name
             post_data.text = request.form['content']
             db.session.commit()
             return redirect(url_for(f'blog_page'))
@@ -170,6 +187,52 @@ with app.app_context():
         db.session.delete(data)
         db.session.commit()
         return redirect(url_for('blog_page'))
+
+    @app.route('/login', methods=['GET', 'POST'])
+    def login_page():
+        form = PostForm()
+        if request.method == 'POST':
+            email = request.form['email']
+            password = request.form['password']
+            try:
+                user = db.session.query(User).filter_by(email=email).first()
+                if check_password_hash(user.password, password):
+                    login_user(user)
+                    return redirect(url_for('blog_page'))
+                else:
+                    flash('Password incorrect, please try again.')
+            except:
+                flash('This email does not exist, please try again.')
+        return render_template('login.html', form=form)
+
+
+    @app.route('/logout')
+    @login_required
+    def logout():
+        logout_user()
+        return redirect(url_for('main_page'))
+
+    @app.route('/register', methods=['GET', 'POST'])
+    def register_page():
+        form = PostForm()
+        if request.method == 'POST':
+            try:
+                name = request.form['name']
+                email = request.form['email']
+                password = request.form['password']
+                password = generate_password_hash(password, method="pbkdf2:sha256", salt_length=8)
+                new_user = User(
+                    name=name,
+                    email=email,
+                    password=password
+                )
+                db.session.add(new_user)
+                db.session.commit()
+                return redirect(url_for('login_page'))
+            except:
+                flash("You've already signed up with that email, log in instead!")
+                return redirect(url_for('login_page'))
+        return render_template('register.html', form=form)
 
 
     @app.route('/easteregg')
