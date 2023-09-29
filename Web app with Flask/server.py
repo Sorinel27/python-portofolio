2ee6a512f21c793e03d5f70c13994e05f4f37b9c
Sorin <sorin78910@gmail.com>
2023-09-29 11:12:37 +0300

    update with special ranks for users (eg. admin, developer) and an admin panel with giving or removing the special statuses

--- Web app with Flask/server.py
+++ Web app with Flask/server.py
@@ -1,7 +1,10 @@
 import requests
 import smtplib
+import os
+import datetime
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
 from flask import Flask, render_template, url_for, request, redirect, flash
 from flask_wtf import FlaskForm, CSRFProtect
 from flask_ckeditor import CKEditorField, CKEditor
@@ -10,13 +13,17 @@ from sqlalchemy.orm import relationship
 from wtforms.validators import DataRequired
 from wtforms import StringField, SubmitField, EmailField
 from flask_bootstrap import Bootstrap
-import datetime
+from PIL import Image
+
 
 app = Flask(__name__)
 NEWS_KEY = '4d8f01d7210c405e907159f8f8e0cd49'
 GET_LINK = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={NEWS_KEY}'
+UPLOAD_FOLDER = f'static/img'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 csrf = CSRFProtect(app)
 app.secret_key = "topsecretkey"
 ckeditor = CKEditor(app)
@@ -42,6 +49,7 @@ with app.app_context():
         content = CKEditorField('Content', validators=[DataRequired()])
         submit = SubmitField('Submit post')
 
+
     class CommentForm(FlaskForm):
         content = CKEditorField('Content', validators=[DataRequired()])
         submit = SubmitField('Add new comment')
@@ -54,8 +62,13 @@ with app.app_context():
         password = db.Column(db.String(100), nullable=False)
         name = db.Column(db.String(100), unique=True, nullable=False)
         profile_views = db.Column(db.Integer, nullable=False)
+        picture_url = db.Column(db.String(3000))
+        has_functions = db.Column(db.Boolean, nullable=False)
+        is_admin = db.Column(db.Boolean, nullable=False)
+        is_developer = db.Column(db.Boolean, nullable=False)
         posts = relationship("BlogPosts", back_populates="author")
         comment = relationship("Comments", back_populates="author")
+
 
     class BlogPosts(UserMixin, db.Model):
         __tablename__ = "posts"
@@ -68,6 +81,7 @@ with app.app_context():
         date = db.Column(db.String(250), nullable=False)
         text = db.Column(db.Text, nullable=False)
         post_views = db.Column(db.Integer, nullable=False)
+
 
     class Comments(UserMixin, db.Model):
         __tablename__ = "comment"
@@ -100,6 +114,10 @@ with app.app_context():
 
     db.create_all()
 
+
+    def allowed_file(filename):
+        return '.' in filename and \
+            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
     @login_manager.user_loader
     def load_user(user_id):
@@ -183,8 +201,9 @@ with app.app_context():
     @app.route('/blog/post/<id>', methods=['POST', 'GET'])
     def post_page(id):
         selected_post = BlogPosts.query.filter_by(id=id).first()
-        selected_post.post_views += 1
-        db.session.commit()
+        if current_user.id != selected_post.user_id:
+            selected_post.post_views += 1
+            db.session.commit()
         post_comments = Comments.query.filter_by(post_id=id).all()
         form = CommentForm()
         if request.method == "POST" and request.form['content'] != '':
@@ -205,7 +224,8 @@ with app.app_context():
             db.session.add(new_comment)
             db.session.commit()
             return redirect(url_for('post_page', id=id))
-        return render_template('post.html', comments=post_comments, form=form, post=selected_post, current_user=current_user)
+        return render_template('post.html', comments=post_comments, form=form, post=selected_post,
+                               current_user=current_user)
 
 
     @app.route('/new-post', methods=['POST', 'GET'])
@@ -225,7 +245,8 @@ with app.app_context():
                 subtitle=request.form['subtitle'],
                 user_id=current_user.id,
                 date=today_date,
-                text=request.form['content']
+                text=request.form['content'],
+                post_views=0
             )
             db.session.add(new_post)
             db.session.commit()
@@ -281,6 +302,7 @@ with app.app_context():
                 flash('This email does not exist, please try again.')
         return render_template('login.html', form=form)
 
+
     @app.route('/logout')
     @login_required
     def logout():
@@ -292,23 +314,35 @@ with app.app_context():
     def register_page():
         form = PostForm()
         if request.method == 'POST':
-            try:
-                name = request.form['name']
-                email = request.form['email']
-                password = request.form['password']
-                password = generate_password_hash(password, method="pbkdf2:sha256", salt_length=8)
-                new_user = User(
-                    name=name,
-                    email=email,
-                    password=password,
-                    profile_views=0
-                )
-                db.session.add(new_user)
-                db.session.commit()
-                return redirect(url_for('login_page'))
-            except:
-                flash("You've already signed up with that email, log in instead!")
-                return redirect(url_for('login_page'))
+            # try:
+            name = request.form['text']
+            email = request.form['email']
+            password = request.form['password']
+            if name == '' or email == '' or password == '':
+                flash("Please fill in all the forms")
+                return redirect(url_for('register_page'))
+            else:
+                if len(email.split('.')) != 2:
+                    flash('Please enter a valid email address!')
+                    return redirect(url_for('register_page'))
+                else:
+                    password = generate_password_hash(password, method="pbkdf2:sha256", salt_length=8)
+                    new_user = User(
+                        name=name,
+                        email=email,
+                        password=password,
+                        profile_views=0,
+                        picture_url=None,
+                        has_functions=False,
+                        is_admin=False,
+                        is_developer=False
+                    )
+                    db.session.add(new_user)
+                    db.session.commit()
+                    return redirect(url_for('login_page'))
+            # except:
+            #     flash("You've already registered with that email, log in instead!")
+            #     return redirect(url_for('login_page'))
         return render_template('register.html', form=form)
 
 
@@ -324,10 +358,24 @@ with app.app_context():
         else:
             return f'<h1 style="color: red;">FORBIDDEN</h1>', 403
 
+
     @app.route('/profile/<name>')
     def user_profile(name):
         wrong_name = False
         user = User.query.filter_by(name=name).first()
+        img_route = None
+        if user.picture_url is not None:
+            im = Image.open(f'static/img/{user.picture_url}')
+            width, height = im.size
+            if width > 700:
+                width = 700
+            if height > 1300:
+                height = 1300
+            size = width, height
+            im_resized = im.resize(size, Image.ANTIALIAS)
+            im_resized.save(f'static/img/{user.picture_url}')
+            if user.picture_url is not None:
+                img_route = f'img/{user.picture_url}'
         user_posts = BlogPosts.query.filter_by(user_id=user.id).all()
         if current_user.is_authenticated:
             try:
@@ -343,8 +391,67 @@ with app.app_context():
                                wrong_name=wrong_name,
                                name=name,
                                user_posts=user_posts,
-                               number_of_posts=len(user_posts)
+                               number_of_posts=len(user_posts),
+                               route=img_route
                                )
+
+
+    @app.route('/profile/<name>/edit', methods=['GET', 'POST'])
+    @login_required
+    def edit_profile(name):
+        user = User.query.filter_by(name=name).first()
+        print(user.picture_url)
+        form = PostForm()
+        if current_user.id == user.id or current_user.id == 1:
+            if request.method == 'POST':
+                file = request.files['file']
+                if file.filename == '':
+                    flash('No selected file')
+                    return redirect(request.url)
+                if file and allowed_file(file.filename):
+                    extension = file.filename.split('.')[1]
+                    file.filename = f'{user.name}.{extension}'
+                    user.picture_url = file.filename
+                    db.session.commit()
+                    filename = secure_filename(file.filename)
+                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                    return redirect(url_for('user_profile', name=name))
+            else:
+                # flash('Error adding profile photo')
+                return render_template('edit-profile.html', form=form)
+        else:
+            return f'<h1 style="color: red; text-align:center;">403 FORBIDDEN</h1>', 403
+
+    @app.route('/panel', methods=['POST', 'GET'])
+    @login_required
+    def panel_page():
+        if current_user.id != 1:
+            return f'<h2 style="color: crimson;">YOU ARE NOT ALLOWED!</h2>', 403
+        else:
+            form = PostForm()
+            if request.method == 'POST':
+                name = request.form['name']
+                user = User.query.filter_by(name=name).first()
+                if user is not None:
+                    if request.form.get('adm_check') is not None:
+                        user.is_admin = True
+                        user.has_functions = True
+                    else:
+                        user.is_admin = False
+                    if request.form.get('dev_check') is not None:
+                        user.is_developer = True
+                        user.has_functions = True
+                    else:
+                        user.is_developer = False
+                    if request.form.get('adm_check') is None and request.form.get('dev_check') is None:
+                        user.has_functions = False
+                    db.session.commit()
+                    return redirect(url_for('user_profile', name=user.name))
+                else:
+                    flash('Wrong name!')
+                    return redirect(url_for('panel_page'))
+            return render_template('panel.html', form=form)
+
 
     @app.route('/easteregg')
     def cool_page():
